e340afc2044ec552cda70679e39b4f8e4f97d865
Pablo Vannini <vanninipablo@gmail.com>
2021-06-13 13:31:52 -0300

    fix type

--- cgi-bin/generateServerProperties.py
+++ cgi-bin/generateServerProperties.py
@@ -8,11 +8,11 @@ import defs
 
 server_properties = ""
 
-with open(defs.MC_WORLDLESS_SP, 'r') as f:
+with open(defs.MC_WORLDLESS_PATH, 'r') as f:
     for l in f:
         server_properties += l
 
-with open(defs.MC_LEVEL_NAME_SP, 'r') as f:
+with open(defs.MC_LEVEL_NAME_PATH, 'r') as f:
     for l in f:
         server_properties += l
 
