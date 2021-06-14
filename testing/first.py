fceb1f850b6eff2d4a983cc93f15a6a3253b591e
Pablo Vannini <vanninipablo@gmail.com>
2021-06-13 22:07:58 -0300

    adding test arguments & check for valid ip

--- testing/first.py
+++ testing/first.py
@@ -2,9 +2,11 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 import time
 import shutil
 import unittest
+import ipaddress
 import subprocess
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -26,47 +28,51 @@ class ScreenProcess:
         subprocess.run(["screen", "-S", self.processName, "-X", 'stuff', '^C'])
 
 class PythonOrgSearch(unittest.TestCase):
+    START_SERVER = True
+    HEADLESS_MODE = True
 
     def setUp(self):
-        self.process = ScreenProcess("testing")
-        self.process.exec("cd ../;python -m http.server --cgi 8000")
+        if self.START_SERVER:
+            self.process = ScreenProcess("testing")
+            self.process.exec("cd ../;python -m http.server --cgi 8000")
         try:
             shutil.rmtree("../minecraft")
         except FileNotFoundError:
             pass
         os.mkdir("../minecraft")
         options = Options()
-        options.headless = True
+        options.headless = self.HEADLESS_MODE
         self.driver = webdriver.Firefox(options=options)
 
     def test_happy_path(self):
         driver = self.driver
         driver.get("http://localhost:8000/")
         self.assertIn("MinecraftServerManager", driver.title)
-        ## wait for update-version-links-button
-        e = (By.ID, "update-version-links-button")
+
         w = WebDriverWait(driver, 200)
+        e = (By.ID, "public-ip")
+        element = w.until(EC.visibility_of_element_located(e))
+
+        # check if it is a valid ip
+        ipaddress.ip_address(element.text)
+
+        e = (By.ID, "update-version-links-button")
         element = w.until(EC.presence_of_element_located(e))
-        #press it
         element.click()
 
-        ## wait for version-links tenga algun link adentro
         e = (By.CSS_SELECTOR, "#version-links > a:nth-child(3)")
         element = w.until(EC.presence_of_element_located(e))
         element.click()
-        
-        # buscar y presionar 1.16.5
-        ## wait hasta que cambie de pagina
+
         e = (By.CSS_SELECTOR, "html body pre") 
         element = w.until(EC.text_to_be_present_in_element(e, "was installed"))
-        # volver atras y refrescar
+
         driver.back()
         driver.refresh()
 
         e = (By.CSS_SELECTOR, "button#start-stop-button") 
         startStopButton = w.until(EC.presence_of_element_located(e))
 
-        ## comprobar que el boton start-stop-button sea "Start"
         e = (By.CSS_SELECTOR, "button#start-stop-button") 
         element = w.until(EC.text_to_be_present_in_element(e, "Start"))
         startStopButton.click()
@@ -89,8 +95,17 @@ class PythonOrgSearch(unittest.TestCase):
 
     def tearDown(self):
         self.driver.close()
-        self.process.keyboardInterrupt()
-        self.process.exec("exit")
+        if self.START_SERVER:
+            self.process.keyboardInterrupt()
+            self.process.exec("exit")
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        PythonOrgSearch.HEADLESS_MODE = sys.argv.pop() != "false"
+    
+    if len(sys.argv) > 1:
+        PythonOrgSearch.START_SERVER = sys.argv.pop() != "false"
+    
+    print("START_SERVER", PythonOrgSearch.START_SERVER)
+    print("HEADLESS_MODE", PythonOrgSearch.HEADLESS_MODE)
     unittest.main()
