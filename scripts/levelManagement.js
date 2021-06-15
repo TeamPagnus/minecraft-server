d92f86e64193a6c6fe84ac38a67aa400efce90ce
absentabyss <agna.lumi@protonmail.com>
2021-06-15 19:30:51 -0300

    [Front] Level management module now handles JSON responses

--- scripts/levelManagement.js
+++ scripts/levelManagement.js
@@ -1,23 +1,122 @@
-function setSelectedLevel(responseJSON) {
-	var level = JSON.parse(responseJSON)["level-name"];
-	document.getElementById("selected-level").innerHTML = level;
-	document.getElementById(
-		"selected-level"
-	).innerHTML += ` <a href="/cgi-bin/downloadLevel.py?level-name=${level}">Download<a>`;
-	document.getElementById(
-		"selected-level"
-	).innerHTML += ` <a href="/cgi-bin/deleteLevel.py?level-name=${level}">Delete<a>`;
+function getSelectedLevel(responseJSON) {
+    var level = JSON.parse(responseJSON)["level-name"];
+    document.getElementById("selected-level").innerHTML = level;
+    var downloadLevelButton = document.getElementById(
+        "selected-level-download"
+    );
+    downloadLevelButton.addEventListener("click", (e) => {
+        var level = document.getElementById("selected-level").innerHTML;
+        var url = `/cgi-bin/downloadLevel.py?level-name=${level}`;
+        var request = new XMLHttpRequest();
+        request.open("GET", url, true);
+        request.onload = function () {
+            var res = JSON.parse(request.responseText);
+            if (res["success"] === "true") {
+                window.open(res["url"]);
+            }
+        };
+        request.onerror = function () {};
+        request.send(); // create FormData from form that triggered event
+        event.preventDefault();
+    });
+    var deleteLevelButton = document.getElementById("selected-level-delete");
+    deleteLevelButton.addEventListener("click", (e) => {
+        var level = document.getElementById("selected-level").innerHTML;
+        var url = `/cgi-bin/deleteLevel.py?level-name=${level}`;
+        var request = new XMLHttpRequest();
+        request.open("GET", url, true);
+        request.onload = function () {
+            var res = JSON.parse(request.responseText);
+            if (res["success"] === "true") {
+                updateLevelModule();
+            }
+        };
+        request.onerror = function () {};
+        request.send(); // create FormData from form that triggered event
+        event.preventDefault();
+    });
 }
 
-function setAvailableLevels(responseJSON) {
-	var levels = JSON.parse(responseJSON)["levels"];
-	for (level of levels) {
-		level_name = level["level-name"];
-		document.getElementById(
-			"available-levels"
-		).innerHTML += `<a href="/cgi-bin/setLevel.py?level-name=${level_name}">${level_name}<a> `;
-	}
+function getAvailableLevels(responseJSON) {
+    var levels = JSON.parse(responseJSON)["levels"];
+    document.getElementById("available-levels").innerHTML = "";
+    for (level of levels) {
+        var level_name = level["level-name"];
+        var buttonId = `level-name=${level_name}`;
+        document.getElementById("available-levels").innerHTML += newButton(
+            buttonId,
+            level_name
+        );
+    }
+    for (level of levels) {
+        var level_name = level["level-name"];
+        var buttonId = `level-name=${level_name}`;
+        var button = document.getElementById(buttonId);
+        button.addEventListener("click", (e) => {
+            var url = `/cgi-bin/setLevel.py?${e["target"]["id"]}`;
+            var request = new XMLHttpRequest();
+            request.open("GET", url, true);
+            request.onload = function () {
+                var res = JSON.parse(request.responseText);
+                if (res["success"] === "true") {
+                    updateLevelModule();
+                }
+            };
+            request.onerror = function () {};
+            request.send(); // create FormData from form that triggered event
+            event.preventDefault();
+        });
+    }
 }
 
-httpGetAsync("/cgi-bin/getCurrentLevel.py", setSelectedLevel);
-httpGetAsync("/cgi-bin/getLevelsList.py", setAvailableLevels);
+function setLevelName(event) {
+    var url = "/cgi-bin/setLevel.py";
+    var request = new XMLHttpRequest();
+    request.open("POST", url, true);
+    request.onload = function () {
+        // request successful
+        // we can use server response to our request now
+        updateLevelModule();
+    };
+
+    request.onerror = function () {
+        // request failed
+    };
+
+    request.send(new FormData(event.target)); // create FormData from form that triggered event
+    event.preventDefault();
+}
+
+function uploadLevel(event) {
+    var url = "/cgi-bin/uploadLevel.py";
+    var request = new XMLHttpRequest();
+    request.open("POST", url, true);
+    request.onload = function () {
+        var res = JSON.parse(request.responseText);
+        if (res["success"] === "true") {
+            document.getElementById(
+                "upload-level-success"
+            ).innerText = `Upload successful. (${getCurrentDatetime()})`;
+            updateLevelModule();
+        }
+    };
+
+    request.onerror = function () {
+        // request failed
+    };
+
+    request.send(new FormData(event.target)); // create FormData from form that triggered event
+    event.preventDefault();
+}
+
+function updateLevelModule() {
+    httpGetAsync("/cgi-bin/getCurrentLevel.py", getSelectedLevel);
+    httpGetAsync("/cgi-bin/getLevelsList.py", getAvailableLevels);
+}
+
+document
+    .getElementById("set-level-name")
+    .addEventListener("submit", setLevelName);
+document.getElementById("upload-level").addEventListener("submit", uploadLevel);
+
+updateLevelModule();
