e0e3ec24acbb03af68c8a866da4c0094ee39a8fd
absentabyss <agna.lumi@protonmail.com>
2021-06-15 00:29:30 -0300

    [Front] Server properties are properly submitted
    Submission button no longer redirects to JSON file.
    Added a timestamp as feedback for successful submission.

--- scripts/serverProperties.js
+++ scripts/serverProperties.js
@@ -1,50 +1,70 @@
 function fillNumbers(key, value) {
-	numberFields = ["max-players", "spawn-protection"];
-	if (numberFields.includes(key)) {
-		document.getElementById(key).value = value;
-	}
+    var numberFields = ["max-players", "spawn-protection"];
+    if (numberFields.includes(key)) {
+        document.getElementById(key).value = value;
+    }
 }
 
 function fillSelects(key, value) {
-	selectFields = ["difficulty"];
-	if (selectFields.includes(key)) {
-		document.getElementById(key).value = value;
-	}
+    var selectFields = ["difficulty"];
+    if (selectFields.includes(key)) {
+        document.getElementById(key).value = value;
+    }
 }
 
 function fillRadios(key, value) {
-	radioFields = [
-		"online-mode",
-		"enable-command-block",
-		"spawn-animals",
-		"spawn-npcs",
-		"force-gamemode",
-		"white-list",
-		"pvp",
-		"allow-flight",
-		"spawn-monsters",
-		"allow-nether",
-	];
-	if (radioFields.includes(key)) {
-		document.getElementById(`${key}-${value}`).checked = true;
-	}
+    var radioFields = [
+        "online-mode",
+        "enable-command-block",
+        "spawn-animals",
+        "spawn-npcs",
+        "force-gamemode",
+        "white-list",
+        "pvp",
+        "allow-flight",
+        "spawn-monsters",
+        "allow-nether",
+    ];
+    if (radioFields.includes(key)) {
+        document.getElementById(`${key}-${value}`).checked = true;
+    }
 }
 
 function fillText(key, value) {
-	textFields = ["resource-pack"];
-	if (textFields.includes(key)) {
-		document.getElementById(key).value = value;
-	}
+    var textFields = ["resource-pack"];
+    if (textFields.includes(key)) {
+        document.getElementById(key).value = value;
+    }
 }
 
 function updateServerProperties(responseJSON) {
-	response = JSON.parse(responseJSON)["server-properties"];
-	for (const [key, value] of Object.entries(response)) {
-		fillNumbers(key, value);
-		fillSelects(key, value);
-		fillRadios(key, value);
-		fillText(key, value);
-	}
+    var response = JSON.parse(responseJSON)["server-properties"];
+    for (const [key, value] of Object.entries(response)) {
+        fillNumbers(key, value);
+        fillSelects(key, value);
+        fillRadios(key, value);
+        fillText(key, value);
+    }
 }
+
+function setServerProperties(event) {
+    var url = "";
+    var request = new XMLHttpRequest();
+    request.open("POST", url, true);
+    request.onload = function () {
+        // request successful
+        // we can use server response to our request now
+        document.getElementById("server-properties-success").innerText = `server.properties saved. (${getCurrentDatetime()})`
+    };
+
+    request.onerror = function () {
+        // request failed
+    };
+
+    request.send(new FormData(event.target)); // create FormData from form that triggered event
+    event.preventDefault();
+}
+
+document.getElementById("server-properties").addEventListener("submit", setServerProperties);
 
 httpGetAsync("/cgi-bin/getServerProperties.py", updateServerProperties);
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,13 +1,44 @@
 function httpGetAsync(theUrl, callback) {
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.onreadystatechange = function () {
-		if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-			callback(xmlHttp.responseText);
-	};
-	xmlHttp.open("GET", theUrl, true); // true for asynchronous
-	xmlHttp.send(null);
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    };
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
 }
 
 function sleep(ms) {
-	return new Promise((resolve) => setTimeout(resolve, ms));
+    return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+function getCurrentDatetime() {
+    var today = new Date();
+    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var dateTime = date+' '+time;
+    return dateTime
+}
+
+/*
+ * // This function serves as a preset for custom form submitting.
+ * function formSubmit(event) {
+ *     var url = "";
+ *     var request = new XMLHttpRequest();
+ *     request.open("POST", url, true);
+ *     request.onload = function () {
+ *         // request successful
+ *         // we can use server response to our request now
+ *         console.log(request.responseText);
+ *     };
+ *
+ *     request.onerror = function () {
+ *         // request failed
+ *     };
+ *
+ *     request.send(new FormData(event.target)); // create FormData from form that triggered event
+ *     event.preventDefault();
+ * }
+ * // It can be attached to a form with the following command.
+ * document.getElementById(formId).addEventListener("submit", formSubmit);
+ */
