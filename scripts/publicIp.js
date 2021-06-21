2955eb5a89ed420d31fcfae1670a690150b9f3a7
absentabyss <agna.lumi@protonmail.com>
2021-06-21 20:16:44 -0300

    Removed unnecessary console logging

--- scripts/publicIp.js
+++ scripts/publicIp.js
@@ -2,6 +2,5 @@ var publicIp = document.getElementById("public-ip");
 
 httpGetAsync("/cgi-bin/getPublicIp.py", function (responseJSON) {
 	var res = JSON.parse(responseJSON)["ip-address"];
-	console.log(res);
 	publicIp.innerText = res;
 });
